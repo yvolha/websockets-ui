1fcd2ee287f41b290fe6663ef6a725f20ad05184
Olya Y <f_akora@mail.ru>
2023-07-12 01:25:53 +0300

    feat: update js to ts and set up environment

--- index.ts
+++ index.ts
@@ -2,5 +2,5 @@ import { httpServer } from "./src/http_server/index.js";
 
 const HTTP_PORT = 8181;
 
-console.log(`Start static http server on the ${HTTP_PORT} port!`);
+console.log(`Started static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
