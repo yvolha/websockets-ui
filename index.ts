306ae195ccd4c370649751a0a5466099f006d8d0
Olya Y <f_akora@mail.ru>
2023-07-14 00:23:00 +0300

    feat: create basic WebSocket server

--- index.ts
+++ index.ts
@@ -1,6 +1,25 @@
 import { httpServer } from "./src/http_server/index.js";
 
+import { WebSocketServer } from "ws";
+
 const HTTP_PORT = 8181;
 
-console.log(`Started static http server on the ${HTTP_PORT} port!`);
-httpServer.listen(HTTP_PORT);
+httpServer.listen(HTTP_PORT, () => {
+  console.log(`Started static http server on localhost:${HTTP_PORT}!`);
+});
+
+const WS_PORT = 3000;
+
+const wss = new WebSocketServer({ port: WS_PORT });
+
+wss.on("listening", () => {
+  console.log(`Started WS server on localhost:${WS_PORT}!`);
+});
+
+wss.on("connection", function connection(ws) {
+  ws.on("error", console.error);
+
+  ws.on("message", function message(data) {
+    console.log("received: %s", data);
+  });
+});
